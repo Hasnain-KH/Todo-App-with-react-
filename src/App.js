e6103abad5a2c55aadb2a19f336b0e9f29a23110
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-08-09 19:22:14 +0500

    Implement basic todo app with add and display features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [inuputValue, setinputValue] = useState("");
+  const [todos, settodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <input value={inuputValue} onChange={(e) => {
+        setinputValue(e.target.value)
+      }} />
+
+      <button onClick={() => {
+        settodos([...todos, inuputValue])
+        setinputValue("");
+
+      }}>Add todo</button>
+
+      <ul>
+        {todos.map((v, i) => {
+          return (
+            <li key={i} >{v} <button>delete</button>  <button>Edit</button></li>
+          )
+        })}
+
+      </ul>
+
     </div>
   );
 }
